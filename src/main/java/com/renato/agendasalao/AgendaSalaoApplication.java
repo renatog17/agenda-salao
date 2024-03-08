3474d2315c67384105daa84be054e3ecb95757f2
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-03-08 01:41:34 -0300

    set up

---
+++ src/main/java/com/renato/agendasalao/AgendaSalaoApplication.java
@@ -0,0 +1,13 @@
+package com.renato.agendasalao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSalaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaSalaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renato/agendasalao/AgendaSalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.agendasalao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSalaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
